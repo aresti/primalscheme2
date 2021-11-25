e5c97b3314f0a5ab47cda93425780a015b7e3ed8
Andrew Smith <andysmith.d@gmail.com>
2021-11-25 22:34:18 +0000

    Decompose ascheme _find_pair; improve method docstrings

--- primaldeep/scheme.py
+++ primaldeep/scheme.py
@@ -1,3 +1,23 @@
+"""
+PrimalScheme: a primer3 wrapper for designing multiplex primer schemes
+
+Copyright (C) 2021 Joshua Quick and Andrew Smith
+www.github.com/aresti/primalscheme
+
+This module contains the Scheme class.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 import csv
 from pathlib import Path
 from typing import Sequence
@@ -17,51 +37,79 @@ class Scheme:
         self.cfg = cfg
         self.pools: Sequence[Sequence[PrimerPair]] = []
 
-    def _find_pairs(
-        self,
-        forward_primer: Primer,
-    ) -> Sequence[PrimerPair]:
+    def reverse_primer_window_start(self, fwd: Primer) -> int:
         """
-        For a given forward primer, return all possible PrimerPairs, sorted by reverse
-        amplicon length.
+        Given a forward Primer, find the start of the window for reverse candidates.
+        Min amplicon size and max primer size determine this bound, relative to the
+        forward primer.
         """
-        window_left = (
-            forward_primer.start + self.cfg.amplicon_size_min - self.cfg.primer_size_max
-        )
-        window_right = forward_primer.start + self.cfg.amplicon_size_max - 1
-        reverse_candidates = [
-            k for k in self.kmers if k.start >= window_left and k.end <= window_right
-        ]
-        candidate_pairs = [
-            PrimerPair(
-                forward_primer,
-                Primer(reverse_complement(r.seq), r.end, PrimerDirection.REVERSE),
-            )
-            for r in reverse_candidates
-        ]
-        candidate_pairs = [
-            pair for pair in candidate_pairs if len(pair) >= self.cfg.amplicon_size_min
-        ]
-        candidate_pairs.sort(key=len, reverse=True)
-        return candidate_pairs
+        return fwd.start + self.cfg.amplicon_size_min - self.cfg.primer_size_max
 
-    @property
+    def reverse_primer_window_end(self, fwd: Primer) -> int:
+        """
+        Given a forward Primer, find the end of the window for reverse candidates.
+        Max amplicon size sets the right bound, relative to the forward primer.
+        """
+        return fwd.start + self.cfg.amplicon_size_max - 1
+
+    def reverse_primer_window(self, fwd: Primer) -> tuple[int, int]:
+        """
+        Given a foward Primer, return reference coords describing a window for
+        possible reverse candidates.
+        """
+        return (
+            self.reverse_primer_window_start(fwd),
+            self.reverse_primer_window_end(fwd),
+        )
+
+    def reverse_candidate_kmers(self, fwd: Primer) -> list[Kmer]:
+        """
+        Given a forward Primer, return a list of Kmers as candidates for a
+        reverse primer, satisfying amplicon size constraints.
+        """
+        window = self.reverse_primer_window(fwd)
+        return [k for k in self.kmers if k.start >= window[0] and k.end <= window[1]]
+
+    def reverse_candidate_pairs(self, fwd: Primer) -> list[PrimerPair]:
+        """
+        Given a forward Primer, return a list of candidate PrimerPairs that
+        satisfy amplicon size constraints, sorted highest-length first.
+        """
+        pairs = [
+            PrimerPair(
+                fwd,
+                Primer(reverse_complement(kmer.seq), kmer.end, PrimerDirection.REVERSE),
+            )
+            for kmer in self.reverse_candidate_kmers(fwd)
+        ]
+
+        # Remove pairs that generate an amplicon size less than min
+        pairs = [pair for pair in pairs if len(pair) >= self.cfg.amplicon_size_min]
+
+        # Reverse length sort
+        pairs.sort(key=len, reverse=True)
+        return pairs
+
     def primer_pairs(self) -> list[PrimerPair]:
+        """
+        Return a flat list of all PrimerParis in the scheme, sorted by start position.
+        """
         return sorted(
             [pair for pool in self.pools for pair in pool],
             key=lambda p: p.forward.start,
         )
 
-    @property
     def primer_bed_rows(
         self,
-    ) -> Sequence[tuple[str, int, int, str, int, str, str]]:
-        """Format primer BED rows."""
-        rows: list[tuple[str, int, int, str, int, str, str]] = []
+    ) -> Sequence[tuple[str, ...]]:
+        """
+        Return BED formatted rows for all primers in the scheme.
+        """
+        rows = []
         ref_id = self.ref.id
         primer_name_template = self.cfg.prefix + "_{}_{}"
 
-        for n, pair in enumerate(self.primer_pairs):
+        for n, pair in enumerate(self.primer_pairs()):
             pool_num = n % len(self.pools) + 1
             for direction in PrimerDirection:
                 name = primer_name_template.format(n, direction.name)
@@ -77,70 +125,80 @@ class Scheme:
                     direction.value,
                     p.seq,
                 )
-                rows.append(row)
+                rows.append(tuple(map(str, row)))
 
         return rows
 
-    @property
-    def primer_bed(self) -> str:
-        rows = ["\t".join(map(str, row)) for row in self.primer_bed_rows]
-        return "\n".join(rows)
-
     def write_primer_bed(self) -> None:
+        """
+        Write a BED file describing all primers in the scheme.
+        """
         filepath = self.cfg.output / f"{self.cfg.prefix}.primer.bed"
 
         with filepath.open("w") as fh:
             cw = csv.writer(fh, delimiter="\t")
-            cw.writerows(self.primer_bed_rows)
+            cw.writerows(self.primer_bed_rows())
 
-    @property
-    def primer_gff(self) -> str:
+    def amplicon_primer_gff_rows(self) -> list[tuple[str, ...]]:
+
         rows = []
         ref_id = self.ref.id
         primer_name_template = self.cfg.prefix + "_{}_{}"
 
-        for n, pair in enumerate(self.primer_pairs):
+        # Amplicons
+        for n, pair in enumerate(self.primer_pairs()):
             pool_num = n % len(self.pools) + 1
             color = "#274e13" if pool_num == 1 else "#16537e"
-            rows.append(
-                [
-                    ref_id,
-                    "primalscheme",
-                    "mRNA",
-                    pair.start + 1,
-                    pair.end + 1,
-                    ".",
-                    ".",
-                    ".",
-                    f"ID=AMP{n+1};Color={color}",
-                ]
-            )
+            row = [
+                ref_id,
+                "primalscheme",
+                "mRNA",
+                pair.start + 1,
+                pair.end + 1,
+                ".",
+                ".",
+                ".",
+                f"ID=AMP{n+1};Color={color}",
+            ]
+            rows.append(tuple(map(str, row)))
+
+            # Primers
             for direction in PrimerDirection:
                 name = primer_name_template.format(n, direction.name)
                 p = getattr(pair, direction.name.lower())
                 start = p.start if p.direction == PrimerDirection.FORWARD else p.end
                 end = p.end if p.direction == PrimerDirection.FORWARD else p.start
-                rows.append(
-                    [
-                        ref_id,
-                        "primalscheme",
-                        "exon",
-                        start + 1,
-                        end + 1,
-                        ".",
-                        ".",
-                        ".",
-                        f"ID={name};Name={name};Parent=AMP{n+1};Note=POOL_{pool_num}",
-                    ]
-                )
-        tsv_rows = ["\t".join(map(str, row)) for row in rows]
+                row = [
+                    ref_id,
+                    "primalscheme",
+                    "exon",
+                    start + 1,
+                    end + 1,
+                    ".",
+                    ".",
+                    ".",
+                    f"ID={name};Name={name};Parent=AMP{n+1};Note=POOL_{pool_num}",
+                ]
+                rows.append(tuple(map(str, row)))
+
+        return rows
+
+    def amplicon_primer_gff(self) -> str:
+        """
+        Return GFF3 formatted multiline str for all amplicons and associated primers in
+        the scheme.
+        """
+        tsv_rows = ["\t".join(map(str, row)) for row in self.amplicon_primer_gff_rows()]
         return "##gff-version 3\n" + "\n".join(tsv_rows)
 
     def write_primer_gff(self) -> None:
+        """
+        Write a GFF3 file for all amplicons and associated primers in the scheme.
+        """
         filepath = self.cfg.output / f"{self.cfg.prefix}.primer.gff"
 
         with filepath.open("w") as fh:
-            fh.write(self.primer_gff)
+            fh.write(self.amplicon_primer_gff())
 
     @property
     def report_filepath(self) -> Path:
@@ -148,12 +206,14 @@ class Scheme:
         return self.cfg.output / f"{self.cfg.prefix}_report.html"
 
     def write_report(self) -> None:
-        """Write HTML report and associated JSON"""
+        """
+        Write HTML report and associated JSON
+        """
 
         # Data JSON
         data = {
             "reference": get_data_uri(self.ref.format("fasta")),
-            "primerTrack": get_data_uri(self.primer_gff),
+            "primerTrack": get_data_uri(self.amplicon_primer_gff()),
         }
 
         # HTML Template
